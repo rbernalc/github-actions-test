0d050fc5bdb592f9d3a56c8943d07ff39a037f40
Bernal Castro <rodrigo.bernal.castro@intel.com>
2022-07-05 17:48:33 -0500

    Using own docker actions

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -1,28 +1,31 @@
 #!/bin.sh -l
 
-#To communicate with the runner is necessary to log something in a specific format as below
-echo "::debug ::Debug Message"
-echo "::warning ::Warning Message"
-echo "::error ::Error Message"
-
-#Set a secret (what will mask a string)
-echo "::add-mask::$1"
-
-
-#show time
+echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
 
-#Add an expandable log
-echo "::group::Some expandable logs"
-echo "Some Stuff1"
-echo "Some Stuff2"
-echo "Some Stuff3"
-echo "::endgroup"
+# #To communicate with the runner is necessary to log something in a specific format as below
+# echo "::debug ::Debug Message"
+# echo "::warning ::Warning Message"
+# echo "::error ::Error Message"
 
-#Export and enviroment variable
-echo "::set-env name=HELLO::hello"
+# #Set a secret (what will mask a string)
+# echo "::add-mask::$1"
 
-#This can be done also in python for example but using print instead of echo
-#print "::debug ::Debug Message"
-echo "Hello $1"
+# #show time
+# time=$(date)
+# echo "::set-output name=time::$time"
+
+# #Add an expandable log
+# echo "::group::Some expandable logs"
+# echo "Some Stuff1"
+# echo "Some Stuff2"
+# echo "Some Stuff3"
+# echo "::endgroup"
+
+# #Export and enviroment variable
+# echo "::set-env name=HELLO::hello"
+
+# #This can be done also in python for example but using print instead of echo
+# #print "::debug ::Debug Message"
+# echo "Hello $1"
