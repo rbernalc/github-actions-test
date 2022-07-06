4f912682eabf779d5113f32d675cba970151e7b5
Bernal Castro <rodrigo.bernal.castro@intel.com>
2022-07-06 09:23:01 -0500

    Using own docker actions final

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -14,3 +14,16 @@ echo "::warning ::This is a warning message"
 
 #Error
 echo "::error ::This is an error message"
+
+#Expandable log
+echo "::group::Some expandable log"
+echo "Log entry number 1"
+echo "Log entry number 2"
+echo "Log entry number 3"
+echo "::endgroup::Final Message"
+
+#Set a secret (This will mask a string to appears as only asterisks)
+echo "::add-mask::$1"
+
+#Show what happen with the mask
+echo "Hello $1"
