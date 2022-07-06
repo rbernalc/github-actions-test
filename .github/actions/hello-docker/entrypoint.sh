1302fc57b4882c0f85a4d8fd7064f8cffb726324
rbernalc <rodrigo.bernal.castro@intel.com>
2022-07-06 09:04:21 -0500

    Update entrypoint.sh

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -19,9 +19,6 @@ echo "Some Stuff2"
 echo "Some Stuff3"
 echo "::endgroup"
 
-#Export and enviroment variable
-echo "::set-env name=HELLO::hello"
-
 #This can be done also in python for example but using print instead of echo
 #print "::debug ::Debug Message"
 echo "Hello $1"
