471910c17b76cd5e38e23578f0d1ee9af6400a2b
Bernal Castro <rodrigo.bernal.castro@intel.com>
2022-07-06 09:18:59 -0500

    Using own docker actions

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -3,3 +3,14 @@
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
+
+#To coomunicate with the runner is necessary to log something in a specific format as below
+
+#Message
+echo "::debug ::This is a debug message"
+
+#Warning
+echo "::warning ::This is a warning message"
+
+#Error
+echo "::error ::This is an error message"
